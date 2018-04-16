e686ee3b80deb637ffc437e667edaf0cd7ebf1b7
Unavailable <andre.hande@hotmail.com>
2018-04-16 12:40:46 +0200

    Sample code

--- src/no/hiof/andreeg/robocode/Badbot.java
+++ src/no/hiof/andreeg/robocode/Badbot.java
@@ -4,5 +4,11 @@ import robocode.Robot;
 
 public class Badbot extends Robot {
 
-    
+    @Override
+    public void run() {
+        while(true) {
+            ahead(100);
+            turnRadarLeft(25);
+        }
+    }
 }
--- src/no/hiof/andreeg/robocode/Main.java
+++ src/no/hiof/andreeg/robocode/Main.java
@@ -3,6 +3,6 @@ package no.hiof.andreeg.robocode;
 public class Main {
 
     public static void main(String[] args) {
-	    //sample text123
+	    Badbot bad = new Badbot();
     }
 }
