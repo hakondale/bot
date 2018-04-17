dd2acb8a8be54519f20aba2d8de1498d5f162bd7
Unavailable <andre.hande@hotmail.com>
2018-04-17 12:04:03 +0200

    Minimal RoboCode Requirement
    Added all of the functions that are necessary to pass the assignment.

--- src/no/hiof/andreeg/robocode/Badbot.java
+++ src/no/hiof/andreeg/robocode/Badbot.java
@@ -1,20 +1,67 @@
 package no.hiof.andreeg.robocode;
 
+import robocode.AdvancedRobot;
 import robocode.HitWallEvent;
 import robocode.Robot;
+import robocode.ScannedRobotEvent;
+import robocode.*;
+import java.util.Random;
 
-public class Badbot extends Robot {
+public class Badbot extends AdvancedRobot {
 
+    /**
+     * Main method of the robot
+     */
     @Override
     public void run() {
-        while(true) {
-            ahead(100);
-            turnRadarLeft(25);
+        Random rand = new Random();
+
+        while (true) {
+
+            int x = rand.nextInt(100) + 50;
+            int turn = rand.nextInt(150) + 45;
+
+            ahead(x);
+            turnRight(turn);
         }
     }
 
+    /**
+     * When the wall is hit
+     * @param event
+     */
     @Override
     public void onHitWall(HitWallEvent event) {
         System.out.println("hit wall");
+        turnRight(180);
+        ahead(150);
+    }
+
+    /**
+     * Robot hit by the opponent
+     * @param event
+     */
+    @Override
+    public void onHitRobot(HitRobotEvent event) {
+        turnRight(75);
+        ahead(300);
+    }
+
+    /**
+     * When the opponent is hit
+     * @param event
+     */
+    @Override
+    public void onBulletHit(BulletHitEvent event) {
+        System.out.println("Opponent hit!");
+    }
+
+    /**
+     * When the opponent robot's location has been determined
+     * @param event
+     */
+    @Override
+    public void onScannedRobot(ScannedRobotEvent event) {
+        fire(1);
     }
 }
